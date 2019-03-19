0b038f9a1e32ba261f82e376ff5c39871083de55
tasano2511 <32722949+tasano2511@users.noreply.github.com>
2019-03-18 20:30:24 -0400

    add new function draw_rectangle()

--- draw_square.py
+++ draw_square.py
@@ -14,7 +14,28 @@ def draw_square():
     brad.right(90)
     brad.forward(100)
     brad.right(90)
-    
     window.exitonclick()
 
+    
+def draw_rectangle():
+
+    window = turtle.Screen()
+    window.bgcolor("blue")
+
+    brad = turtle.Turtle()
+    brad.forward(100)
+    brad.right(90)
+    brad.forward(10)
+    brad.right(90)
+    brad.forward(100)
+    brad.right(90)
+    brad.forward(10)
+    brad.right(90)
+    
+    window.exitonclick()    
+    
+    
+    
+    
 draw_square()
+draw_rectangle()
